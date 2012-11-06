aa26a2c8dfffb4f390aa418fbce687d7b80621b5
Luc Perkins <luc@appfog.com>
2012-11-06 14:59:46 -0800

    OK

--- app.js
+++ app.js
@@ -1,19 +1,86 @@
-var connection = require('amqp').createConnection({ host: 'localhost', port: 5672 });
-var count = 0;
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    url = require('url'),
+    amqp = require('amqp');
 
-connection.on('ready', function(){
-	console.log('Connected to ' + connection.serverProperties.product);
-	var e = connection.exchange('test-exchange');
-	var q = connection.queue('test-queue');
-	q.on('queueDeclareOk', function(args){
-		console.log('Queue opened');
-		q.bind(e, '#');
-		q.on('queueDeclareOk', function(args){
-			console.log('Queue bound');
-			setInterval(function(){
-				console.log('Publishing message #' + ++count);
-				e.publish('routingKey', {count: count});
-			}, 1000);
-		});
-	});
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.VCAP_APP_PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.connectionStatus = 'No server connection';
+app.exchangeStatus = 'No exchange established';
+app.queueStatus = 'No queue established';
+
+app.get('/', function(req, res){
+  res.render('index.jade',
+    { 
+      title: 'Welcome to RabbitMQ and Node/Express on AppFog!',
+      connectionStatus: app.connectionStatus,
+      exchangeStatus: app.exchangeStatus,
+      queueStatus: app.queueStatus
+    });
+});
+
+function connectionUrl(){
+  if (process.env.VCAP_SERVICES){
+    conf = JSON.parse(process.env.VCAP_SERVICES);
+    return conf['rabbitmq-2.4'][0].credentials.url;
+  } else {
+    return 'amqp://localhost';
+  }
+}
+
+app.post('/start-server', function(req, res){
+  app.rabbitMqConnection = amqp.createConnection({ url: connectionUrl() });
+
+  app.rabbitMqConnection.on('ready', function(){
+    app.connectionStatus = 'Connected!';
+    res.redirect('/');
+  });
+});
+
+app.post('/new-exchange', function(req, res){
+  app.e = app.rabbitMqConnection.exchange('test-exchange');
+  app.exchangeStatus = 'An exchange has been established!';
+  res.redirect('/');
+});
+
+app.post('/new-queue', function(req, res){
+  app.q = app.rabbitMqConnection.queue('test-queue');
+  app.queueStatus = 'The queue is ready for use!';
+  res.redirect('/');
+});
+
+app.get('/message-service', function(req, res){
+  app.q.bind(app.e, '#');
+  res.render('message-service.jade',
+    {
+      title: 'Welcome to the messaging service!',
+      sentMessage: ''
+    });
+});
+
+app.post('/newMessage', function(req, res){
+  var newMessage = req.body.newMessage;
+  app.e.publish('routingKey', { message: newMessage });
+
+  app.q.subscribe(function(msg){  
+    var messageToDisplay = msg.message;
+    res.render('message-service.jade',
+      {
+        title: 'You\'ve got mail!',
+        sentMessage: messageToDisplay
+      });
+  });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
 });
